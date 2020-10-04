452a3f380ed4a1ab17879298948f4652d830606a
Dan <daci8242@gmail.com>
2020-10-04 18:10:17 -0400

    make new quote button change quote

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("Trashman js loaded");
+function updateQuoteText(newQuote) {
+	document.getElementById("greenman").innerHTML = newQuote;
+};
+var quoteArray = [
+	"She was the best hoor I ever knew",
+	"I just want to be pure",
+	"I am a Golden God!",
+	"Whoops, I seemed to have dropped my monster condom that I use on my magnum dong!"
+];
+function pickRandomQuote() {
+	return quoteArray[Math.floor(Math.random() * quoteArray.length)];
+};
+function newQuote() {
+	updateQuoteText(pickRandomQuote());
+};
